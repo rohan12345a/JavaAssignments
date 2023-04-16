31a014a74bfd3da7efb759ac6f99cbf6d091d340
Rohan Saraswat <109196424+rohan12345a@users.noreply.github.com>
2023-04-16 18:55:10 +0530

    Add files via upload

---
+++ Assignment6/Main.java
@@ -0,0 +1,31 @@
+package Assignment6;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\n Mallard Duck :\n");
+        MallardDuck mallardDuck=new MallardDuck();
+        mallardDuck.display();
+        mallardDuck.performQuack();
+        mallardDuck.performFly();
+        mallardDuck.performSwim();
+        System.out.println("\n Rubber Duck: \n");
+        RubberDuck rubberDuck=new RubberDuck();
+        rubberDuck.display();
+        rubberDuck.performQuack();
+        rubberDuck.performFly();
+        rubberDuck.performSwim();
+        System.out.println("\nRedhead Duck:\n");
+        RedHeadDuck redheadDuck=new RedHeadDuck();
+        redheadDuck.display();
+        redheadDuck.performQuack();
+        redheadDuck.performFly();
+        redheadDuck.performSwim();
+        System.out.println("\nDecoy Duck:\n");
+        DecoyDuck decoyDuck=new DecoyDuck();
+        decoyDuck.display();
+        decoyDuck.performQuack();
+        decoyDuck.performFly();
+        decoyDuck.performSwim();
+
+    }
+}
